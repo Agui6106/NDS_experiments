cc7fba48091e50fbe5921d5041157e3defaba6af
U-AZUKI606\albet <A01735612@tec.mx>
2025-09-12 21:45:57 +0900

    Agrega archivos para desarollo de la etapa 1

---
+++ experiments/etapa_1/source/main.cpp
@@ -0,0 +1,4 @@
+// Librerias requeridas de devkitPro/libnds
+#include <nds.h>     // Librería principal de libnds
+// Librerias de terceros
+#include <stdio.h>   // Para usar iprintf (texto en consola)
